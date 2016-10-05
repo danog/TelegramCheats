dbd01ce41b47bc946ad3dcf5d458f07f21518009
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-10-05 02:14:16 +0200

    Final tweaks

--- cheats.js
+++ cheats.js
@@ -1,41 +1,41 @@
-function load() {
-    var url = $("#url")
-        .val();
-    var score = $("#score")
-        .val();
-    writeres("Error");
+function cheat(url, score) {
+    if (url === '') {
+        Materialize.toast('No url was provided!', 4000);
+        return false;
+    }
+    if (!$.isNumeric(score)) {
+        Materialize.toast('No score was provided!', 4000);
+        return false;
+    }
     if (url.match(/tbot\.xyz/)) {
-        var param = url.replace(/^.*#/g, '').replace(/\?.*$/g, '');
-        var data = "data=" + param + "&score=" + score;
-        post("https://tbot.xyz/api/setScore", data, function(data) { writeres("OK"); }, function(data) { writeres("Error"); }, true);
-        return;
+        post("https://tbot.xyz/api/setScore", "data=" + url.replace(/^.*#/g, '').replace(/\?.*$/g, '') + "&score=" + score, function(data) { writeok(); }, function(data) { writeres('Error!'); }, true);
+    } else if (url.match(/www\.gameeapp\.com/)) {
+        post("https://bots.gameeapp.com/set-web-score-qkfnsog26w7173c9pk7whg0iau7zwhdkfd7ft3tn", '{ "score": ' + score + ', "url": "' + url.replace(/#.*$/g, '').replace(/.*\//g, '') + '", "play_time": 100}',  function(data) { writeok(); }, function(data) { writeres('Error!'); });
+    } else {
+        Materialize.toast("I cannot use cheats on this game.", 4000);
+        writeres("I cannot use cheats on this game. You can add support to new games by submitting a pull request to <a href='https://github.com/danog/telegramcheats' class='grey-text text-lighten-3' target='_blank'>the TelegramCheats repo.</a>");
     }
-    if (url.match(/www\.gameeapp\.com/)) {
-        param = url.replace(/#.*$/g, '').replace(/.*\//g, '');
-        data =
-            '{ "score": ' + score +
-            ', "url": "' + param +
-            '", "play_time": 100}';
-        post("https://bots.gameeapp.com/set-web-score-qkfnsog26w7173c9pk7whg0iau7zwhdkfd7ft3tn", data, function(data) { writeres("OK"); }, function(data) { writeres("Error"); });
-        return; 
-    }
-    writeres("I cannot use cheats on this game. You can add support to new games by submitting a pull request to <a href='https://github.com/danog/telegramcheats' class='grey-text text-lighten-3' target='_blank'>the TelegramCheats repo</a>");
 }
 function post(url, data, cb, failCb, proxy = false) {
+    writeres('<div class="preloader-wrapper small active"> <div class="spinner-layer spinner-white-only"> <div class="circle-clipper left"> <div class="circle"></div> </div><div class="gap-patch"> <div class="circle"></div> </div><div class="circle-clipper right"> <div class="circle"></div> </div> </div> </div>');
     if (proxy) {
         url = "https://proxy.daniil.it/?url=" + encodeURIComponent(url);
     }
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            var resp = xhr.responseText
-            cb(JSON.parse(resp))
+            cb(JSON.parse(xhr.responseText));
         } else if (failCb) {
-            failCb()
+            failCb();
         }
-    }
+    };
     xhr.open("POST", url, true);
     xhr.send(data);
+    writeres('');
+}
+function writeok() {
+    writeres('OK!');
+    Materialize.toast('OK!', 4000);
 }
 function writeres(cos) {
     $("#res").html(cos);
